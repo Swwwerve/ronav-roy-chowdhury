816cf580ba41f883dfc84c2b6df6ad291aa1378e
Swwwerve <roychr2@mcmaster.ca>
2025-01-01 17:03:53 -0500

    Fixed mobile view for project technologies

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -40,7 +40,9 @@ const Projects = () => {
                             </a>
                             <p className='mb-4 text-neutral-400'>{project.description}</p>
                             {project.technologies.map((tech, index) => (
-                                <span key={index} className='mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900'>
+                                <span 
+                                    key={index} 
+                                    className='mr-1 rounded bg-neutral-900 px-1 py-0.5 text-xs font-medium text-purple-900 sm:mr-2 sm:px-2 sm:py-1 sm:text-sm'>
                                     {tech}
                                 </span>
                             ))}
