49762426a71a95b7cb8324bd31be2028aef018ce
Swwwerve <roychr2@mcmaster.ca>
2025-11-28 17:09:22 -0500

    Navbar changes v5

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <div className='fixed top-0 -z-10 h-full w-full'>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>      </div>
       <Navbar />
-      <div className='container mx-auto px-8 pt-24'>
+      <div className='container mx-auto px-8 pt-16'>
         <Hero />
         <About />
         <Technologies />
